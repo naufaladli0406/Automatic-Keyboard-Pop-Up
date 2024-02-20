77efac1f9c7307f96310759a8ffa6f8be57988c5
Muhammad Naufal Adli <naufaladli0406@gmail.com>
2024-02-20 15:27:35 +0700

    Initial Commit

---
+++ DynamicKeyboard/DynamicKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicKeyboardApp.swift
+//  DynamicKeyboard
+//
+//  Created by Naufal Adli on 20/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DynamicKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
